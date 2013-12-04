ff481dfd50700ef921e0deef0beaf0fb9df4648c
calum <calum@calumroy.com>
2013-12-04 07:26:14 -0800

    First commit of HTM balancer. This projects attempts to balance an inverted pendulum using HTM neural network. This version uses a very simplified pendulum at the moment and simply rewards cells when the angle is closer to vertical.
    The top rows of cells are the angle measurements. The middle cells represent
    close to 90 degrees (vertical) orientation. The second set of rows represents
    the feedback command from higher levels. The bottom set of rows is the
    angle command outputs for that level.
    
    Only level 0 commands are actually used to control the simulated inverted
    pendulum.

---
+++ Balancer/Inverted_Pendulum.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+
+"""
+This code simulates an inverted pendulum balancing on a moving stand.
+The HTM controls the motion of the stand and hence this affects the pendulum.
+
+author: Calum Meiklejohn
+website: calumroy.com
+last edited: August 2013
+"""
+import numpy as np
+import math
+import random
+
+def createInput(angle, gridWidth, gridHeight, angleOverlap, minAcc, maxAcc):
+        # Create the angle input matrix
+        # angle = The current angle of the inverted pendulum
+        # gridWidth = The width of the matrix
+        # gridHeight = The height of the matrix
+        # angleOverlap = The number of columns the active angle cells take up. This affects the overlap between angle readings.
+        # minAcc = The minimum acceleration. This is the acceleration value that the first column represents. 
+        # maxAcc = The maximum acceleration. This is the acceleration value that the last column represents. 
+        if angle=='none':
+            return np.array([[0 for i in range(gridWidth)] for j in range(gridHeight)])
+        angleInput = np.array([[0 for i in range(gridWidth)] for j in range(gridHeight)])
+        anglePos = float(gridWidth)/float(abs(minAcc)+abs(maxAcc))*float(abs(angle-minAcc))
+        #print"anglePos = %s, angleOverlap = %s"%(anglePos,angleOverlap)
+        #angleCol = int(round(anglePos))
+        for row in range(len(angleInput)):
+            for col in range(len(angleInput[0])):
+                if col > (anglePos-angleOverlap) and col < (anglePos+angleOverlap):
+                    angleInput[row][col] = 1
+        #print "grid = ",angleInput
+        return angleInput
+
+        
+        
+
+
+class InvertedPendulum():
+    def __init__(self):
+        self.length = 1.0
+        self.weigth = 1.0
+        self.cartAcc = 1.0   # The carts acceleration in the horizontal direction m/s.
+        self.x = 0.0
+        self.y = self.length
+        self.angle = 90      # 90 deg is upright
+
+    def step(self, acc, time):
+        # Calculate the new position of the pendulum after the time while applying the specified acceleration.
+        # Simple pendulum for now! ]
+        print "self.angle = %s, time = %s, acc = %s"%(self.angle,time,acc)
+        self.angle = self.angle+time*acc
+        return self.angle
+
+
