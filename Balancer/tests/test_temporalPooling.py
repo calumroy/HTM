d7c4e3dab700713912e146cc4d1798b219c5b9b6
calumroy <calum@calumroy.com>
2015-01-17 20:54:57 -0800

    Update the temporal pooler integration tests. This test may need to be put in its own folder or not use the GUI.

--- Balancer/tests/test_temporalPooling.py
+++ Balancer/tests/test_temporalPooling.py
@@ -2,22 +2,16 @@ from mock import MagicMock
 from mock import patch
 from HTM_Balancer import HTM, HTMLayer, HTMRegion, Column
 import numpy as np
+import GUI_HTM
 
 
-class test_TemporalPooling:
-    def setUp(self):
-        self.width = 10
-        self.height = 10
-        self.cellsPerColumn = 3
-
-        # Create an array of input which will be fed to the htm so it
-        # can try to temporarily pool them.
-        self.numInputs = 10
+class simpleVerticalLineInputs:
+    def __init__(self):
         self.inputs = np.array([[[0 for i in range(self.width * self.cellsPerColumn)]
                                 for j in range(self.height)] for k in range(self.numInputs)])
-
-        self.setInputs(self.inputs)
-        self.htmlayer = HTMLayer(self.inputs[0], self.width, self.height, self.cellsPerColumn)
+        self.setInputs()
+        # Use an index to keep track of which input to send next
+        self.index = 0
 
     def setInputs(self, inputs):
         # Will will create vertical lines in the input that move from side to side.
@@ -27,6 +21,33 @@ class test_TemporalPooling:
                 for x in range(len(inputs[n][y])):
                     if x == n:
                         inputs[n][y][x] = 1
+
+    def createSimGrid(self):
+        newGrid = self.inputs[self.index]
+        self.index += 1
+        if (self.index > len(self.inputs)):
+            self.index = 0
+        return newGrid
+
+
+class test_TemporalPooling:
+    def setUp(self):
+        self.width = 10
+        self.height = 10
+        self.cellsPerColumn = 3
+        self.numLevels = 1
+
+        # Create an array of input which will be fed to the htm so it
+        # can try to temporarily pool them.
+        self.numInputs = 10
+
+        self.setInputs(self.inputs)
+        self.InputCreator = simpleVerticalLineInputs()
+        #self.htmlayer = HTMLayer(self.inputs[0], self.width, self.height, self.cellsPerColumn)
+        self.htm = HTM_V.HTM(self.numLevels, self.InputCreator.createSimGrid(), self.width, self.height, self.cellsPerColumn)
+        self.htmGui = GUI_HTM.HTMGui(self.htm, self.InputCreator)
+
+
 
     def test_case1(self):
         pass
