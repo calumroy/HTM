e81768e6a5a84b24974dba25bf244b4d386b0984
Calum Meiklejohn <calum@calumroy.com>
2015-08-10 09:08:05 +0800

    Added class setup and test Main function to learning class.

--- Balancer/HTM_Classes/learning.py
+++ Balancer/HTM_Classes/learning.py
@@ -14,9 +14,10 @@ to speed up the computation. Can be implemented on a GPU
 see theano documents for enabling GPU calculations.
 
 This class requires as inputs:
-    * The current permanence values.
+    * The current permanence values for each cols potential synapse.
     * A list of the connected and unconnected potential synpases for
         each column.
+    * A list of the active columns.
     * How much to increment or decrement synapse values.
 
 '''
@@ -31,15 +32,51 @@ class LearningCalculator():
         ############################################
         # Create the theano function for calculating
         # the new permanence values for each columns spatial synpases.
-        self.col_PotInputsMat = T.matrix(dtype='float32')
+        self.col_PotInputsMat = T.matrix(dtype='int32')
         self.col_SynPermMat = T.matrix(dtype='float32')
-        self.check_gt_zero = T.switch(T.gt(self.colOMat, 0), 1, 0)
-        self.check_gteq_minLocAct = T.switch(T.ge(self.colOMat, self.minLocalActivity), self.check_gt_zero, 0)
+        self.col_ActiveColVect = T.vector(dtype='int32')
+        self.col_num2 = T.matrix(dtype='int32')
+        self.check_inputAct = T.switch(T.gt(self.colOMat, 0), 1, 0)
+        self.check_gtZero = T.switch(T.gt(self.col_ActiveColVect[self.col_num2-1], 0),
+                                     self.check_inputAct, 0)
         #self.indexActCol = tensor.eq(self.check_gteq_minLocAct, 1).nonzero()
-        self.get_updatedSynPerm = function([self.colOMat,
-                                            self.minLocalActivity],
-                                           self.check_gteq_minLocAct,
-                                           on_unused_input='warn',
-                                           allow_input_downcast=True
-                                           )
+        self.get_updateSynPerm = function([self.col_SynPermMat,
+                                           self.col_PotInputsMat,
+                                           self.col_ActiveColVect,
+                                           self.col_num2],
+                                          self.check_gtZero,
+                                          on_unused_input='warn',
+                                          allow_input_downcast=True
+                                          )
+
+        #### END of Theano functions and variables definitions
+        #################################################################
+        # Create a matrix that just holds the column number for each element
+        self.col_num = np.array([[i for i in range(self.potentialWidth*self.potentialHeight)]
+                                for j in range(self.width*self.height)])
+
+    def updatePermanenceValues(self, colSynPerm, colPotInputs, activeCols):
+        pass
+
+
+if __name__ == '__main__':
+
+    numRows = 4
+    numCols = 4
+    spatialPermanenceInc = 0.1
+    spatialPermanenceDec = 0.01
+    numPotSyn = 9
+    numColumns = numRows * numCols
+    # Create an array representing the permanences of colums synapses
+    colSynPerm = np.random.rand(numColumns, numPotSyn)
+    # Create an array representing the potential inputs to each column
+    colSynPerm = np.random.randint(2, size=(numRows, numCols))
+    # Create an array representing the active columns
+    activeCols = np.random.randint(2, size=(numColumns))
+
+    permanenceUpdater = LearningCalculator(spatialPermanenceInc,
+                                           spatialPermanenceDec)
+
+
+
 
