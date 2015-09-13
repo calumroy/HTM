33c6f409f3439f7b1d0a8fdc732eaa6f5788c3b9
Calum Meiklejohn <calum@calumroy.com>
2015-09-13 13:51:35 +0800

    Uncommented enable GUI from temporal testing.

--- Balancer/tests/test_temporalPooling/test_temporalPooling.py
+++ Balancer/tests/test_temporalPooling/test_temporalPooling.py
@@ -256,9 +256,9 @@ class test_temporalPooling:
                 tempPoolPercent[layer] = self.temporalPoolingMeasures[layer].temporalPoolingPercent(gridOutput)
                 #print "Layer %s Temporal pooling percent = %s" % (layer, tempPoolPercent[layer])
 
-        app = QtGui.QApplication(sys.argv)
-        self.htmGui = GUI_HTM.HTMGui(self.htm, self.InputCreator)
-        sys.exit(app.exec_())
+        # app = QtGui.QApplication(sys.argv)
+        # self.htmGui = GUI_HTM.HTMGui(self.htm, self.InputCreator)
+        # sys.exit(app.exec_())
 
         # Less then this percentage of temporal pooling should have occurred
         for i in range(len(tempPoolPercent)):
