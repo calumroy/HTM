56a95fc2c8342abc2c69b48c6cee69ded7717d3e
calumroy <calum@calumroy.com>
2015-09-21 05:27:31 -0700

    Added test for theano overlap with uncentered potential synapses. Not finished yet.

--- Balancer/HTM_Classes/tests/test_theano_overlap.py
+++ Balancer/HTM_Classes/tests/test_theano_overlap.py
@@ -7,6 +7,7 @@ import json
 from copy import deepcopy
 from HTM_Classes import theano_overlap
 
+
 class test_theanoOverlap:
     def setUp(self):
         '''
@@ -121,5 +122,109 @@ class test_theanoOverlap:
 
         #import ipdb; ipdb.set_trace()
         assert np.sum(colOverlaps) == 0
+
+    def test_uncenteredCase1(self):
+        '''
+        Test the theano overlap calculator with a case where
+        each column calculates the overlap with that columns
+        potential synpases begining from the top right. The
+        potential synpases are not cenetered around the column.
+        '''
+        potWidth = 2
+        potHeight = 2
+        centerPotSynapses = 0
+        numColumnRows = 4
+        numColumnCols = 4
+        connectedPerm = 0.3
+        minOverlap = 3
+
+        colSynPerm = np.array([[0, 0, 0, 0],
+                               [0, 0, 0, 0],
+                               [0, 0, 0, 0],
+                               [0, 0, 0, 0],
+                               [0, 0, 0, 0],
+                               [0, 0, 0, 0],
+                               [0, 0, 0, 0],
+                               [0, 0, 0, 0],
+                               [1, 1, 1, 1],
+                               [1, 1, 1, 1],
+                               [1, 1, 1, 1],
+                               [1, 1, 1, 1],
+                               [0, 0, 0, 0],
+                               [0, 0, 0, 0],
+                               [0, 0, 0, 0],
+                               [0, 0, 0, 0]])
+
+        newInputMat = np.array([[1, 1, 1, 1],
+                                [0, 0, 0, 0],
+                                [1, 1, 1, 1],
+                                [0, 0, 0, 0]])
+
+        numInputCols = 4
+        numInputRows = 4
+
+        # Create an instance of the overlap calculation class
+        overlapCalc = theano_overlap.OverlapCalculator(potWidth,
+                                                       potHeight,
+                                                       numColumnCols,
+                                                       numColumnRows,
+                                                       numInputCols,
+                                                       numInputRows,
+                                                       centerPotSynapses,
+                                                       connectedPerm,
+                                                       minOverlap)
+
+        # Return both the overlap values and the inputs from
+        # the potential synapses to all columns.
+        colOverlaps, colPotInputs = overlapCalc.calculateOverlap(colSynPerm, newInputMat)
+
+        # limit the overlap values so they are larger then minOverlap
+        colOverlaps = overlapCalc.removeSmallOverlaps(colOverlaps)
+
+        #import ipdb; ipdb.set_trace()
+        assert np.sum(colOverlaps) == 0
+
+    def test_uncenteredInputSizes(self):
+        potWidth = 4
+        potHeight = 4
+        centerPotSynapses = 0
+        numColumnRows = 7
+        numColumnCols = 5
+        connectedPerm = 0.3
+        minOverlap = 3
+        numPotSyn = potWidth * potHeight
+        numColumns = numColumnRows * numColumnCols
+
+        # Create an array representing the permanences of colums synapses
+        colSynPerm = np.random.rand(numColumns, numPotSyn)
+
+        for i in range(4, 100, 3):
+            numInputRows = i
+            for j in range(4, 100, 7):
+                numInputCols = j
+                print "NEW TEST ROUND"
+                print "numInputRows, numInputCols = %s, %s " % (numInputRows, numInputCols)
+                newInputMat = np.random.randint(2, size=(numInputRows, numInputCols))
+                # Create an instance of the overlap calculation class
+                overlapCalc = theano_overlap.OverlapCalculator(potWidth,
+                                                               potHeight,
+                                                               numColumnCols,
+                                                               numColumnRows,
+                                                               numInputCols,
+                                                               numInputRows,
+                                                               centerPotSynapses,
+                                                               connectedPerm,
+                                                               minOverlap)
+
+                columnPotSynPositions = overlapCalc.getPotentialSynapsePos(numInputCols, numInputRows)
+
+                # Return both the overlap values and the inputs from
+                # the potential synapses to all columns.
+                colOverlaps, colPotInputs = overlapCalc.calculateOverlap(colSynPerm, newInputMat)
+
+                # limit the overlap values so they are larger then minOverlap
+                colOverlaps = overlapCalc.removeSmallOverlaps(colOverlaps)
+
+                assert len(colOverlaps) == numColumns
 
 
