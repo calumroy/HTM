9da6795d59395a0d3701c275477b36ac66fa57fc
Calum Meiklejohn <calum@calumroy.com>
2015-09-28 20:59:59 +0800

    Started to make tests for testing uncentered potential synapses setting in the then calculator classes. Deleted unused params file.

--- Balancer/HTM_Classes/tests/test_theano_overlap.py
+++ Balancer/HTM_Classes/tests/test_theano_overlap.py
@@ -122,4 +122,51 @@ class test_theanoOverlap:
         #import ipdb; ipdb.set_trace()
         assert np.sum(colOverlaps) == 0
 
+    # def test_nonCenteredInputSizes(self):
+    #     '''
+    #     Test the theano overlap calculator with a range of input sizes
+    #     where the potential synapses are not cenetered around the column.
+    #     '''
+    #     potWidth = 4
+    #     potHeight = 4
+    #     centerPotSynapses = 0
+    #     numColumnRows = 7
+    #     numColumnCols = 5
+    #     connectedPerm = 0.3
+    #     minOverlap = 3
+    #     numPotSyn = potWidth * potHeight
+    #     numColumns = numColumnRows * numColumnCols
+
+    #     # Create an array representing the permanences of colums synapses
+    #     colSynPerm = np.random.rand(numColumns, numPotSyn)
+
+    #     for i in range(4, 100, 3):
+    #         numInputRows = i
+    #         for j in range(4, 100, 7):
+    #             numInputCols = j
+    #             print "NEW TEST ROUND"
+    #             print "numInputRows, numInputCols = %s, %s " % (numInputRows, numInputCols)
+    #             newInputMat = np.random.randint(2, size=(numInputRows, numInputCols))
+    #             # Create an instance of the overlap calculation class
+    #             overlapCalc = theano_overlap.OverlapCalculator(potWidth,
+    #                                                            potHeight,
+    #                                                            numColumnCols,
+    #                                                            numColumnRows,
+    #                                                            numInputCols,
+    #                                                            numInputRows,
+    #                                                            centerPotSynapses,
+    #                                                            connectedPerm,
+    #                                                            minOverlap)
+
+    #             columnPotSynPositions = overlapCalc.getPotentialSynapsePos(numInputCols, numInputRows)
+
+    #             # Return both the overlap values and the inputs from
+    #             # the potential synapses to all columns.
+    #             colOverlaps, colPotInputs = overlapCalc.calculateOverlap(colSynPerm, newInputMat)
+
+    #             # limit the overlap values so they are larger then minOverlap
+    #             colOverlaps = overlapCalc.removeSmallOverlaps(colOverlaps)
+
+    #             assert len(colOverlaps) == numColumns
+
 
