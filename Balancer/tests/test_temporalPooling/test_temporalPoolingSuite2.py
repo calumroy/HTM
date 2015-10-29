e51b3ae11a15f29378dba92d88552a49479c3ee3
Calum Meiklejohn <calum@calumroy.com>
2015-10-29 22:43:14 +0800

    Changed spatial pooling decrement to increase the temporal pooling. A way needs to be found to tune temporal pooling without adjusting the spatial permanence inc and dec values. See Issue #18

--- Balancer/tests/test_temporalPooling/test_temporalPoolingSuite2.py
+++ Balancer/tests/test_temporalPooling/test_temporalPoolingSuite2.py
@@ -99,8 +99,8 @@ testParameters = {
                                     'inhibitionRadius': 1,
                                     'potentialWidth': 30,
                                     'potentialHeight': 10,
-                                    'spatialPermanenceInc': 0.1,
-                                    'spatialPermanenceDec': 0.01,
+                                    'spatialPermanenceInc': 0.2,
+                                    'spatialPermanenceDec': 0.005,
                                     'permanenceInc': 0.1,
                                     'permanenceDec': 0.005,
                                     'minDutyCycle': 0.01,
@@ -209,9 +209,9 @@ class test_temporalPoolingSuite2:
                 tempPoolPercent[layer] = self.temporalPoolingMeasures[layer].temporalPoolingPercent(gridOutput)
                 #print "Layer %s Temporal pooling percent = %s" % (layer, tempPoolPercent[layer])
 
-        # app = QtGui.QApplication(sys.argv)
-        # self.htmGui = GUI_HTM.HTMGui(self.htm, self.InputCreator)
-        # sys.exit(app.exec_())
+        app = QtGui.QApplication(sys.argv)
+        self.htmGui = GUI_HTM.HTMGui(self.htm, self.InputCreator)
+        sys.exit(app.exec_())
 
         # Less then this percentage of temporal pooling should have occurred
         for i in range(len(tempPoolPercent)):
